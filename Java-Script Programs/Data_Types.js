ec4fc254ce5aa93eea76ef9105613689d1bf9fb9
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-09 13:41:30 +0530

    Add files via upload

---
+++ Java-Script Programs/Data_Types.js
@@ -0,0 +1,57 @@
+console.log("Jay Shree Ganesh");
+
+
+/*
+    Types of Data Types
+
+    1. Primitive
+    2. Object
+    3. Sepecial Data Types
+
+*/
+
+/* 1. Primitive Data Types
+        A. Number
+        B. String
+        C. Boolean
+        D. Undefined
+        E. Null
+        F. Symbol
+*/
+
+// A. Number
+// Represents numeric values.
+// Example
+    let num=5;
+    console.log("Number is "+num);
+
+// B. String
+// Represents sequences of characters.
+// Example
+    let text="Thakur";
+    console.log("String is "+text);
+
+// C. Boolean
+// Represents either true or false.
+//Example
+    let isTrue=true;
+    console.log("Boolean "+isTrue);
+
+// D. Undefined
+// Represents an uninitialized variable or a function without a return value.
+//Example
+    let x;
+
+// E. Null
+// Represents the intentional absence of any object value.
+//Example
+    let y=null;
+
+// E. Symbol
+// Represents a unique identifier
+// Example
+    let sym= Symbol('unique');
+    console.log(sym);
+
+console.log("---------------(JAVA SCRIPT DATA TYPES)---------------");
+
