722c06e587a56cb0f8e792bf1256a33af420c8f4
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-10 10:04:36 +0530

    Add files via upload

---
+++ Java-Script Programs/Conditional Statements/If.js
@@ -0,0 +1,24 @@
+
+
+/*
+    A. 
+    1. If Conditional Statement
+    2. If Else Conditional Statement
+    3. If Else If Condition Statement 
+    4. Nested
+    
+*/
+
+// 1. If Conditional Statement
+// The if statement is the most basic form of a conditional statement. It executes a block of code if a specified condition evaluates to true.
+//Example 
+const temp = 25;
+if (temp > 20)
+    console.log("It's a warm day!!");
+
+let isLogged = true;
+if (isLogged) {
+    console.log("User is logged in.");
+}
+
+//---------------------------------------------------------------------
---
+++ Java-Script Programs/Conditional Statements/Types_Of_Conditional_Statement.js
@@ -0,0 +1,19 @@
+
+
+/*
+    Conditional Statements
+
+    Types of Conditional Statements
+
+    A.
+        1. If
+        2. If Else
+        3. If Else If
+        4. Nested
+
+    B.
+        1. Switch Case
+        2. Nested
+*/
+
+console.log("---------------------Conditional Statement---------------------");
