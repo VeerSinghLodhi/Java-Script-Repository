cb61bce29c2356c8426ac01e708993114789502d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-11 21:18:02 +0530

    Update Function_Expressions.js

--- Java-Script Programs/Functions/Function_Expressions.js
+++ Java-Script Programs/Functions/Function_Expressions.js
@@ -2,3 +2,26 @@
 /*
     Function Expression
 */
+// Example 1
+
+const getPrintVeer=function(){
+    console.log("Veer Singh Lodhi");
+}
+getPrintVeer();
+console.log("--------------------------");
+
+// Example 2
+
+const getSum=function(num1,num2){
+    return num1+num2;
+}
+console.log("Sum is "+getSum(30,30));
+console.log("---------------------------");
+
+// Example 3
+ 
+const getProduct=function(num1,num2){
+    return num1*num2;
+}
+console.log("Product is "+getProduct(10,20));
+console.log("------------------------------");
