1b11fb2bcea2959312bb36f54211b57a863a736f
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-11 21:19:40 +0530

    Add files via upload

---
+++ Java-Script Programs/Functions/Arrow_Functions.js
@@ -0,0 +1,30 @@
+/*
+    Arrow Functions
+*/
+
+// Example 1
+
+const getPrint=()=>{
+    console.log("Veer Singh Lodhi");
+};
+
+getPrint();
+console.log("--------------------------");
+
+// Example 2 
+
+const getSum=(num1,num2)=>{
+    return num1+num2;
+};
+
+console.log("Sum is "+getSum(10,20));
+console.log("-----------------------------");
+
+// Example 3
+
+const getProduct=(num1,num2)=>{
+    return num1*num2;
+}
+console.log("Product is "+getProduct(10,30));
+
+console.log("--------------------------");
